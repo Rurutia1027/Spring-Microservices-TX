4cfdae5f482eb16e2b393e1b7a092a3d154ac170
Rurutia1027 <rurutia1027@gmail.com>
2025-03-28 20:22:29 +0800

    add message queue jms notification logic to the provider side

--- distributed-sys-payments/service-message-provider/src/main/java/com/cloud/payment/service/message/api/impl/RpcRpTransactionMessageServiceImpl.java
+++ distributed-sys-payments/service-message-provider/src/main/java/com/cloud/payment/service/message/api/impl/RpcRpTransactionMessageServiceImpl.java
@@ -72,8 +72,19 @@ public class RpcRpTransactionMessageServiceImpl implements RpcRpTransactionMessa
         // refresh message record to db
         rpTransactionMessageService.updateMessage(message);
 
-        // todo: send message to active|kafka queue
-        activeMQMessageQueueService.sendMessage("topic", "message status");
+        if (StringUtils.isBlank(message.getConsumerQueue())) {
+            throw new MessageBizException(MessageBizException.MESSAGE_CONSUMER_QUEUE_IS_NULL,
+                    "Received message id " + messageId + " queried entity message consumer " +
+                            "queue is blank!");
+        }
+
+        if (StringUtils.isBlank(message.getMessageBody())) {
+            throw new MessageBizException(MessageBizException.SAVED_MESSAGE_IS_NULL,
+                    "Saved message entity's body field is blank! Meaningless message will " +
+                            "not be sent to the consumer queue");
+        }
+
+        activeMQMessageQueueService.sendMessage(message.getConsumerQueue(), message.getMessageBody());
     }
 
     @Override
@@ -94,11 +105,26 @@ public class RpcRpTransactionMessageServiceImpl implements RpcRpTransactionMessa
         message.setEditTime(new Date());
         RpTransactionMessage ret = rpTransactionMessageService.updateMessage(message);
 
-        // send message queue message here
+
+        // send message notification to consumer queue
+        String consumerQueue = message.getConsumerQueue();
+        String messageBody = message.getMessageBody();
+
+        if (StringUtils.isBlank(consumerQueue)) {
+            throw new MessageBizException(MessageBizException.MESSAGE_CONSUMER_QUEUE_IS_NULL,
+                    "Received message consumer queue name is blank!");
+        }
+
+        if (StringUtils.isBlank(messageBody)) {
+            throw new MessageBizException(MessageBizException.SAVED_MESSAGE_IS_NULL,
+                    "Received message body is blank!");
+        }
+
+        activeMQMessageQueueService.sendMessage(consumerQueue, messageBody);
         return Objects.nonNull(ret) && ret.getId() > 0 ? 0 : -1;
     }
 
-    // Sending message out directly without store message to database table
+    // Deliver message directly to the consumer queue without store message to database table
     @Override
     public void directSendMessage(RpTransactionMessage message) throws MessageBizException {
         if (Objects.isNull(message)) {
@@ -106,12 +132,21 @@ public class RpcRpTransactionMessageServiceImpl implements RpcRpTransactionMessa
                     "Received to be saved message is null!");
         }
 
-        if (StringUtils.isEmpty(message.getConsumerQueue())) {
-            throw new MessageBizException(MessageBizException.MESSAGE_CONSUMER_QUEUE_IS_NULL
-                    , "Received msg consume queue is not defined!");
+        String messageBody = message.getMessageBody();
+        String consumerQueue = message.getConsumerQueue();
+
+
+        if (StringUtils.isEmpty(consumerQueue)) {
+            throw new MessageBizException(MessageBizException.MESSAGE_CONSUMER_QUEUE_IS_NULL,
+                    "Received msg consume queue is not defined!");
         }
 
-        // todo: send message to message queue
+        if (StringUtils.isEmpty(messageBody)) {
+            throw new MessageBizException(MessageBizException.SAVED_MESSAGE_IS_NULL,
+                    "Received message body is blank!");
+        }
+
+        activeMQMessageQueueService.sendMessage(consumerQueue, messageBody);
     }
 
     @Override
@@ -122,15 +157,22 @@ public class RpcRpTransactionMessageServiceImpl implements RpcRpTransactionMessa
         }
 
         if (StringUtils.isEmpty(message.getConsumerQueue())) {
-            throw new MessageBizException(MessageBizException.MESSAGE_CONSUMER_QUEUE_IS_NULL
-                    , "Received message's consumer queue name is empty");
+            throw new MessageBizException(MessageBizException.MESSAGE_CONSUMER_QUEUE_IS_NULL,
+                    "Received message's consumer queue name is empty");
+        }
+
+        if (StringUtils.isEmpty(message.getMessageBody())) {
+            throw new MessageBizException(MessageBizException.SAVED_MESSAGE_IS_NULL,
+                    "Received or stored message's body is blank!");
         }
 
         message.addSendTimes();
         message.setEditTime(new Date());
         rpTransactionMessageService.updateMessage(message);
 
-        // todo: send jms message
+        // after update the message items in storage layer, continue to re-deliver the
+        // message to message queue
+        activeMQMessageQueueService.sendMessage(message.getConsumerQueue(), message.getMessageBody());
     }
 
     @Override
@@ -143,8 +185,7 @@ public class RpcRpTransactionMessageServiceImpl implements RpcRpTransactionMessa
                     " find message entity from db by given messageId " + messageId);
         }
 
-        // todo: refine this latter, set max retry times to config file
-
+        // todo: refine this latter, set max retry times to config fil
         int maxReSendTimes = 5;
         if (message.getMessageSendTimes() >= maxReSendTimes) {
             message.setAlreadyDead(PublicEnum.YES.name());
@@ -154,7 +195,20 @@ public class RpcRpTransactionMessageServiceImpl implements RpcRpTransactionMessa
         message.setMessageSendTimes(message.getMessageSendTimes() + 1);
         rpTransactionMessageService.updateMessage(message);
 
-        // todo: send jms message
+        String consumerQueue = message.getConsumerQueue();
+        String messageBody = message.getMessageBody();
+
+        if (StringUtils.isBlank(consumerQueue)) {
+            throw new MessageBizException(MessageBizException.MESSAGE_CONSUMER_QUEUE_IS_NULL,
+                    "Message's inner consumer message queue name is blank!");
+        }
+
+        if (StringUtils.isBlank(messageBody)) {
+            throw new MessageBizException(MessageBizException.SAVED_MESSAGE_IS_NULL,
+                    "Message's inner message body is blank!");
+        }
+
+        activeMQMessageQueueService.sendMessage(consumerQueue, messageBody);
     }
 
     @Override
