fac9bad5ef1e210961c32a6f9c120ae62c0c0299
Rurutia1027 <rurutia1027@gmail.com>
2025-04-02 22:51:40 +0800

    remove rpc client side configure server port value

--- cloud-payment/message-web/docker-run.sh
+++ cloud-payment/message-web/docker-run.sh
@@ -1,9 +1,7 @@
 #!/bin/zsh
 
-docker run -d --name message-service \
-    -p 59081:59081 \  # Maps the internal message-service rpc client port to the host
-    -p 8580:8580 \    # Maps the web application port
-    -e SERVER_PORT=8580 \  # Sets the server ports as an environment variable
-    -e MESSAGE_SERVICE_HOST=localhost \  # Sets the service host
-    -e MESSAGE_SERVICE_PORT=59081 \      # Sets service port
-    docker.io/nanachi1027/message-service:latest
+docker run -d --name message-web \
+    -p 8580:8580 \
+    -e SERVER_PORT=8580 \
+    -e MESSAGE_SERVICE_HOST=localhost \
+    docker.io/nanachi1027/message-web:latest
