fc79a8e80c6fbe7e58e592ec7e4463fd7575a863
Rurutia1027 <rurutia1027@gmail.com>
2025-04-03 16:42:32 +0800

    use spring cloud gateway to let client side find services, let server side register its service to spring cloud gateway

---
+++ cloud-payment/gateway-service/src/main/java/com/cloud/payment/gateway/controller/FacadeController.java
@@ -0,0 +1,15 @@
+package com.cloud.payment.gateway.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/facade")
+public class FacadeController {
+    @GetMapping("/status")
+    public ResponseEntity<String> getStatus() {
+        return ResponseEntity.ok("Success");
+    }
+}
