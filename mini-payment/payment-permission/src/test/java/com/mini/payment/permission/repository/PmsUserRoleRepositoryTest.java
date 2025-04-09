8df097fade9cea871e5dc0ede775af9d6af96660
Rurutia1027 <rurutia1027@gmail.com>
2025-04-09 20:59:04 +0800

    add test cases for permission repos

--- mini-payment/payment-permission/src/test/java/com/mini/payment/permission/repository/PmsUserRoleRepositoryTest.java
+++ mini-payment/payment-permission/src/test/java/com/mini/payment/permission/repository/PmsUserRoleRepositoryTest.java
@@ -1,6 +1,10 @@
 package com.mini.payment.permission.repository;
 
 import com.mini.payment.PaymentPermissionApplicationTest;
+import com.mini.payment.permission.PmsMockUtils;
+import com.mini.payment.permission.entity.PmsRole;
+import com.mini.payment.permission.entity.PmsUser;
+import com.mini.payment.permission.entity.PmsUserRole;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +15,40 @@ public class PmsUserRoleRepositoryTest {
     @Autowired
     private PmsUserRoleRepository pmsUserRoleRepository;
 
+    @Autowired
+    private PmsUserRepository pmsUserRepository;
+
+    @Autowired
+    private PmsRoleRepository pmsRoleRepository;
+
     @Test
     public void initTest() {
         Assertions.assertNotNull(pmsUserRoleRepository);
+    }
+
+    @Test
+    public void saveAndQuery() {
+        PmsUser pmsUser = PmsMockUtils.mockPmsUser();
+        PmsUser pmsUserRet = pmsUserRepository.save(pmsUser);
+        Assertions.assertTrue(pmsUserRet.getId() > 0);
+
+        PmsRole pmsRole = PmsMockUtils.mockPmsRole();
+        PmsRole pmsRoleRet = pmsRoleRepository.save(pmsRole);
+        Assertions.assertTrue(pmsRoleRet.getId() > 0);
+
+        PmsUserRole pmsUserRole = new PmsUserRole();
+        pmsUserRole.setRole(pmsRole);
+        pmsUserRole.setUser(pmsUser);
+
+        PmsUserRole pmsUserRoleRet = pmsUserRoleRepository.save(pmsUserRole);
+        Assertions.assertTrue(pmsUserRet.getId() > 0);
+        PmsUserRole pmsUserRoleQueryRet =
+                pmsUserRoleRepository.findById(pmsUserRoleRet.getId()).orElse(null);
+        Assertions.assertNotNull(pmsUserRoleQueryRet);
+
+        pmsUserRepository.delete(pmsUserRet);
+        pmsUserRoleQueryRet =
+                pmsUserRoleRepository.findById(pmsUserRoleRet.getId()).orElse(null);
+        Assertions.assertNull(pmsUserRoleQueryRet);
     }
 }
