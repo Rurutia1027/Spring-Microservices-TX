c318dfccab2887e38446e6bcf554c3b06ef9f316
Rurutia1027 <rurutia1027@gmail.com>
2025-04-12 18:17:03 +0800

    add http utils to NotifyTask

--- mini-payment/payment-app-notify/src/main/java/com/mini/payment/app/notify/core/NotifyTask.java
+++ mini-payment/payment-app-notify/src/main/java/com/mini/payment/app/notify/core/NotifyTask.java
@@ -1,16 +1,21 @@
 package com.mini.payment.app.notify.core;
 
-import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.mini.payment.app.notify.entity.MpNotifyRecord;
 import com.mini.payment.app.notify.entity.NotifyStrategy;
 import com.mini.payment.app.notify.enums.NotifyStatusEnum;
 import com.mini.payment.exception.BizException;
+import com.mini.payment.utils.httpclient.HttpParam;
+import com.mini.payment.utils.httpclient.HttpResult;
+import com.mini.payment.utils.httpclient.HttpUtils;
 import org.apache.hc.core5.http.HttpStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.concurrent.Delayed;
 import java.util.concurrent.TimeUnit;
 
@@ -62,18 +67,18 @@ public class NotifyTask implements Runnable, Delayed {
             LOG.info("Notify Url: " + notifyRecord.getUrl() + " ;notify id: "
                     + notifyRecord.getId() + ";notify times:" + notifyRecord.getNotifyTimes());
 
-            // todo: implement http client and wrap inner params
+            HttpParam httpParam = new HttpParam(notifyRecord.getUrl());
+            HttpResult httpResult = HttpUtils.httpRequest(httpParam);
 
 
             notifyRecord.setNotifyTimes(notifyTimes + 1);
             String successValue = notifyStrategy.getSuccessValue();
             String responseMsg = "";
-            Integer responseStatus = null;
+            Integer responseStatus = httpResult.getStatusCode();
 
             // here we fetch response status and those status determines
             // the record's inner information value
-            if (Objects.nonNull(null) && ((responseStatus == 200 || responseStatus == 201 || responseStatus == 202 || responseStatus == 203
-                    || responseStatus == 204 || responseStatus == 205 || responseStatus == 206))) {
+            if (Objects.nonNull(null) && isStatusCodeValid(responseStatus)) {
                 // notify success, update the notify record's success status to db
                 if (responseMsg.trim().equals(successValue)) {
                     notifyPersist.updateNotifyRecord(String.valueOf(notifyRecord.getId()),
@@ -86,8 +91,7 @@ public class NotifyTask implements Runnable, Delayed {
                             notifyRecord.getNotifyTimes(),
                             NotifyStatusEnum.HTTP_REQUEST_SUCCESS.name());
                 }
-                LOG.info("Update NotifyRecord: " + JSON.toJSONString(notifyRecord)
-                        + "; responseMsg:" + responseMsg);
+                LOG.info("Update NotifyRecord:{};responseMsg:{}", JSONObject.toJSONString(notifyRecord), responseMsg);
             } else {
                 // notify failed, add the record item to message local list
                 // the list will modify record status, and determine whether this record
@@ -116,5 +120,19 @@ public class NotifyTask implements Runnable, Delayed {
                     notifyRecord.getMerchantOrderNo(),
                     notifyRecord.getUrl(), "", HttpStatus.SC_METHOD_FAILURE);
         }
+    }
+
+    private static final Set<Integer> VALID_HTTP_STATUS_CODES = new HashSet<Integer>() {{
+        add(HttpStatus.SC_OK);                   // 200
+        add(HttpStatus.SC_CREATED);              // 201
+        add(HttpStatus.SC_ACCEPTED);             // 202
+        add(HttpStatus.SC_NON_AUTHORITATIVE_INFORMATION); // 203
+        add(HttpStatus.SC_NO_CONTENT);           // 204
+        add(HttpStatus.SC_RESET_CONTENT);        // 205
+        add(HttpStatus.SC_PARTIAL_CONTENT);      // 206
+    }};
+
+    private boolean isStatusCodeValid(Integer responseStatus) {
+        return VALID_HTTP_STATUS_CODES.contains(responseStatus);
     }
 }
