bb938b73b0d1cfb0560699ca17a096743f0999be
Rurutia1027 <rurutia1027@gmail.com>
2025-04-15 19:33:28 +0800

    add order polling listener logic

--- mini-payment/payment-app-notify/src/main/java/com/mini/payment/config/ActiveMqListenerConfig.java
+++ mini-payment/payment-app-notify/src/main/java/com/mini/payment/config/ActiveMqListenerConfig.java
@@ -1,14 +1,10 @@
 package com.mini.payment.config;
 
-import com.mini.payment.app.notify.message.ConsumerMessageListener;
-import jakarta.jms.ConnectionFactory;
 import jakarta.jms.Queue;
 import org.apache.activemq.command.ActiveMQQueue;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.listener.DefaultMessageListenerContainer;
 
 @Configuration
 public class ActiveMqListenerConfig {
--- mini-payment/payment-app-polling/src/main/java/com/mini/payment/app/polling/OrderPollingRunner.java
+++ mini-payment/payment-app-polling/src/main/java/com/mini/payment/app/polling/OrderPollingRunner.java
@@ -3,14 +3,21 @@ package com.mini.payment.app.polling;
 import com.mini.payment.app.polling.core.PollingPersist;
 import com.mini.payment.app.polling.core.PollingTask;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Component;
 
+import java.util.Objects;
 import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class OrderPollingRunner {
+    private static final Logger LOG = LoggerFactory.getLogger(OrderPollingRunner.class);
 
     @Autowired
     private ThreadPoolTaskExecutor threadPool;
@@ -28,10 +35,21 @@ public class OrderPollingRunner {
     public void init() {
         cachePollingPersist = pollingPersist;
         cacheThreadPool = threadPool;
-
-        startThread();
+        startHandleTasks();
     }
 
-    private void startThread() {
+    private void startHandleTasks() {
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        scheduler.scheduleAtFixedRate(() -> {
+            LOG.info("ThreadPool#ActiveThreadCount {}", cacheThreadPool.getActiveCount());
+            LOG.info("ThreadPool#MaxThreadSize {}", cacheThreadPool.getMaxPoolSize());
+
+            if (!tasks.isEmpty() && cacheThreadPool.getActiveCount() < cacheThreadPool.getMaxPoolSize()) {
+                PollingTask task = tasks.poll();
+                if (task != null) {
+                    cacheThreadPool.execute(task);
+                }
+            }
+        }, 0, 100, TimeUnit.MILLISECONDS);
     }
 }
