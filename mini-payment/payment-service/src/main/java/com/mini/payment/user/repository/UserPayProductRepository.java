5aeb5a167f451e3f40ec0914367c5f1cd8bbe4d2
Rurutia1027 <rurutia1027@gmail.com>
2025-04-15 11:04:47 +0800

    updaet user entity, repository and service

--- mini-payment/payment-service/src/main/java/com/mini/payment/user/repository/UserPayProductRepository.java
+++ mini-payment/payment-service/src/main/java/com/mini/payment/user/repository/UserPayProductRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserPayProductRepository extends BaseRepository<UserPayProduct> {
+
 }
--- mini-payment/payment-service/src/main/java/com/mini/payment/user/repository/UserQuickPayBankAccountRepository.java
+++ mini-payment/payment-service/src/main/java/com/mini/payment/user/repository/UserQuickPayBankAccountRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserQuickPayBankAccountRepository extends BaseRepository<UserQuickPayBankAccount> {
+
 }
